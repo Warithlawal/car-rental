43d0e2d49ea9271310d715685c31a6a4ae697edc
Warith Lawal <79901863+Warithlawal@users.noreply.github.com>
2024-09-06 00:38:17 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -331,7 +331,12 @@ faqs.forEach((faq) => {
     });
 });
 
+const scrollUp = () => {
+	const scrollUp = document.getElementById('scroll-up');
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll') : scrollUp.classList.remove('show-scroll')
+};
 
+window.addEventListener('scroll', scrollUp);
 
 
 
